996e8680b195a73bdb88c780340dae199898d162
mdleion9 <mdleionmdleion50@gmail.com>
2026-04-01 21:39:54 +0600

    Cart section compelat

--- src/components/Home/Cart/Cart.jsx
+++ src/components/Home/Cart/Cart.jsx
@@ -1,1 +1,91 @@
- 
+import React from "react";
+import emptyPng from "../../../assets/products/shopping-cart.png";
+import { MdShoppingCart } from "react-icons/md";
+import { toast } from "react-toastify";
+
+const Cart = ({ cartData, setCartData }) => {
+  const totalPrice = cartData.reduce((sum, current) => sum + current.price, 0);
+
+  const handleRemove = (item) => {
+    const filteredData = cartData.filter(
+      (filteredItem) => filteredItem.id !== item.id
+    );
+    if (filteredData) {
+      setCartData(filteredData);
+      toast.info(`${item.name} removed from cart.`, {
+        position: "top-center",
+      });
+    }
+  };
+
+  const handlePayment = () => {
+    setCartData([]);
+    toast.success("Payment Successful! Thank you .", {
+      position: "top-center",
+    });
+  };
+
+  return (
+    <div className="container lg:max-w-7xl mx-auto mb-10 md:mb-20 ">
+      <div className="px-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center mb-4">
+          Your Cart
+        </h2>
+
+        {cartData.length === 0 ? (
+          <div className="flex flex-col items-center justify-center py-20 bg-white border rounded-xl border-zinc-200">
+            <MdShoppingCart className="w-20 h-20 animate-pulse" />
+            <p className="text-xl font-bold text-zinc-400">
+              Your cart is empty
+            </p>
+          </div>
+        ) : (
+          <div className="space-y-2 ">
+            
+            {cartData.map((item) => (
+              <div
+                key={item.id}
+                className="flex justify-between items-center border border-zinc-200 rounded-xl p-2 md:p-3"
+              >
+                <div className="flex gap-2 items-center">
+                  <img
+                    className="w-20 border border-zinc-100 rounded-full p-2"
+                    src={item.icon}
+                    alt={item.name}
+                  />
+                  <div>
+                    <h2>{item.name}</h2>
+                    <p>${item.price}</p>
+                  </div>
+                </div>
+
+                <button
+                  onClick={() => handleRemove(item)}
+                  className="btn btn-sm text-red-500"
+                >
+                  Remove
+                </button>
+              </div>
+            ))}
+
+            <h2 className="flex items-center justify-between">
+              <span className="text-xl text-zinc-500">Total:</span>
+              <span className="text-2xl font-bold">
+                ${totalPrice.toFixed(2)}
+              </span>
+            </h2>
+
+            <button
+              onClick={handlePayment}
+              className="btn w-full rounded-full bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white"
+            >
+              Proceed to Checkout
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
