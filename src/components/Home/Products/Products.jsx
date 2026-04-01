6ff1756f3c5f50ec5987a9a8b8f9738c0447e071
mdleion9 <mdleionmdleion50@gmail.com>
2026-04-01 21:45:01 +0600

    Products section

--- src/components/Home/Products/Products.jsx
+++ src/components/Home/Products/Products.jsx
@@ -1,1 +1,25 @@
- 
+import { useEffect, useState } from "react";
+import Product from "../Product/Product";
+
+const Products = ({ promiseData, cartData, setCartData }) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    promiseData.then(res => setData(res));
+  }, [promiseData]);
+
+  return (
+    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 container lg:max-w-7xl mx-auto py-4 px-4">
+      {data.map(item => (
+        <Product
+          key={item.id}
+          item={item}
+          cartData={cartData}
+          setCartData={setCartData}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Products;
