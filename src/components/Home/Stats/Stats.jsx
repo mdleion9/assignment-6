4b8780c28afb3580a9e66afaca0620f7766b4bbd
mdleion9 <mdleionmdleion50@gmail.com>
2026-04-01 21:46:27 +0600

    Stats section

--- src/components/Home/Stats/Stats.jsx
+++ src/components/Home/Stats/Stats.jsx
@@ -1,1 +1,24 @@
- 
+import React from 'react';
+
+const Stats = () => {
+    return (
+        <div className=' bg-linear-to-r from-[#4F39F6] to-[#9514FA] py-5 md:py-8 lg:py-12 '>
+        <div className='flex items-center justify-between text-center container lg:max-w-7xl mx-auto py-4 md:py-5 lg:py-7 px-4 md:px-6 lg:px-8 text-white'>
+            <div >
+                <h2 className='text-2xl md:text-4xl lg:text-5xl font-bold'>50k+</h2>
+                <p className='lg:text-lg'>Active Users</p>
+            </div>
+            <div>
+                <h2 className='text-2xl md:text-4xl lg:text-5xl font-bold'>200+</h2>
+                <p className='lg:text-lg'>Premium Tools</p>
+            </div>
+            <div>
+                <h2 className='text-2xl md:text-4xl lg:text-5xl font-bold'>4.9</h2>
+                <p className='lg:text-lg'>Rating</p>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default Stats;
